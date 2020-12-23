6f650666ce9e7f897dccd0be9fc98e614db6ca83
Patrycja Pyrek <patipyrek@gmail.com>
2020-12-23 14:28:59 +0100

    Main Window, dodanie EntityFramework

--- LibraryProject/MainWindow.xaml.cs
+++ LibraryProject/MainWindow.xaml.cs
@@ -24,5 +24,25 @@ namespace LibraryProject
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void zwrot(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
